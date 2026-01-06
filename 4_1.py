10c42bf932f53c29ebaf0b181780270849853c39
Mahan Mazaheri <mahan78ir@gmail.com>
2026-01-06 12:45:49 +0330

    improve solution

--- 4_1.py
+++ 4_1.py
@@ -3,7 +3,14 @@ def rec_sum(arr,n):
         return 0
     return rec_sum(arr , n-1) + arr[n-1]
 
+
 array = [1,2,3,4]
 
 print(rec_sum(array,4))
         
+#! For when we want to use another function to find the length of an array
+
+def arraysum(arr):
+    return rec_sum(arr , len(arr))
+
+print(arraysum(array))
